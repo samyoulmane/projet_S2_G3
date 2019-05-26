71a5e5dcfd2a8c466e9e56dfda2b50cdd333a51d
Samy Oulmane <smoulmane@gmail.com>
2019-05-26 19:16:41 +0200

    Ajout graph cor

--- brouillons/brouillon_diag.R
+++ brouillons/brouillon_diag.R
@@ -1,13 +1,1 @@
 
-
-cor_data <- cor(wdbc[,c(-1, -2)])
-
-col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
-corrplot(cor_data, method = "color", col = col(200),
-         type = "upper", order = "hclust", number.cex = .7,
-         addCoef.col = "black", # Add coefficient of correlation
-         tl.col = "black", tl.srt = 90, # Text label color and rotation
-         # Combine with significance
-         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
-         # hide correlation coefficient on the principal diagonal
-         diag = FALSE)
